a77e52c8b1eec5ecf66d50875f6c56c94ac99d99
Nareerat <nareerat.srisai@gmail.com>
2022-01-25 23:08:15 +0100

    Add controller

---
+++ Umbraco_9Project.Core/ViewModel/TwitterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco_9Project.Core.ViewModel
+{
+    public class TwitterViewModel
+    {
+        public string TwitterHandle { get; set; }
+        public bool Error { get; set; }
+        public string Json { get; set; }
+        public string Message { get; set; }
+    }
+}
